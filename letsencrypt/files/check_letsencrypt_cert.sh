576b2bb36d1a7dbf5a9f6cc51df8c81ced8d27b4
Damien Tardy-Panis <damien@m2mobi.com>
2016-04-20 10:31:36 +0200

    use FIRST_CERT variable in check_letsencrypt_cert.sh script

--- letsencrypt/files/check_letsencrypt_cert.sh
+++ letsencrypt/files/check_letsencrypt_cert.sh
@@ -4,9 +4,9 @@ FIRST_CERT=$1
 
 for DOMAIN in "$@"
 do
-    openssl x509 -in /etc/letsencrypt/live/$1/cert.pem -noout -text | grep DNS:${DOMAIN} > /dev/null || exit 1
+    openssl x509 -in /etc/letsencrypt/live/$FIRST_CERT/cert.pem -noout -text | grep DNS:${DOMAIN} > /dev/null || exit 1
 done
-CERT=$(date -d "$(openssl x509 -in /etc/letsencrypt/live/$1/cert.pem -enddate -noout | cut -d'=' -f2)" "+%s")
+CERT=$(date -d "$(openssl x509 -in /etc/letsencrypt/live/$FIRST_CERT/cert.pem -enddate -noout | cut -d'=' -f2)" "+%s")
 CURRENT=$(date "+%s")
 REMAINING=$((($CERT - $CURRENT) / 60 / 60 / 24))
 [ "$REMAINING" -gt "30" ] || exit 1
