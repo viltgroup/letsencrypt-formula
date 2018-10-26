43a7dee5a61b8a754edbfe8b8cb04e094edc0c16
Nicole Scherfenber <nicole.scherfenberg@sinnerschrader.com>
2018-10-26 21:52:23 +0200

    uses inspec instead of serverspec
    tests added for letsencrypt.config

--- Gemfile
+++ Gemfile
@@ -7,5 +7,4 @@ gem "kitchen-docker"
 gem "kitchen-salt", ">=0.1.0"
 gem "kitchen-inspec"
 gem 'inspec'
-gem "serverspec"
 
--- test/integration/deb/serverspec/letsencrypt_spec.rb
+++ test/integration/deb/serverspec/letsencrypt_spec.rb
@@ -1,7 +1,16 @@
-require 'serverspec'
+describe file('/opt/letsencrypt') do
+ it { should_not exist }
+end
 
-# Required by serverspec
-set :backend, :exec
+describe file('/etc/letsencrypt/cli.ini') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_readable }
+    its('size') { should be > 1 }
+    its('content') { should match 'server = https://acme-staging.api.letsencrypt.org/directory' }
+    its('content') { should match 'authenticator = webroot' }
+end
 
 describe file('/usr/bin/letsencrypt') do
   it { should be_symlink }
